93c476e56358162842788664218690303f0d8af6
Isaac Katana <162148948+isaackatana@users.noreply.github.com>
2025-01-12 16:19:54 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Backend server
+        target: 'https://tana-tech-ssfj.onrender.com', // Backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional, if you want to rewrite the path
       },
