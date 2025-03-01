85e9eb804edf9fe4c5ef0ae84e3a63cf0ad07f1c
Isaac Katana <mrisaackatana@gmail.com>
2025-03-01 10:11:48 +0300

    1st

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <img src="https://images.stockcake.com/public/9/a/5/9a5b33df-e8ca-48c5-bc08-789789b47e7f_large/recording-studio-tech-stockcake.jpg" alt="" />
 
       <div className='hero-details'>
-        <h1>Book Your Session</h1>
+        <h1>Record Your Session</h1>
         <p>Audio and Video Production Services</p>
         <button>Book Now</button>
       </div>
