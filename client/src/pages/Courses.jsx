f0db89b7588a48fa41ae59685d06d894f396375d
isaackatana <mrisaackatana@gmail.com>
2025-03-08 11:38:04 +0300

    courses

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -8,9 +8,14 @@ function Courses() {
       <div className="courses">
         <span><Link to='/'><FaArrowLeft/></Link>
         <div>
+          <button>Graphics Design</button>
           <button>Video Editing</button>
-          <button>Mixing & Mastering</button>
+          <button>Web Design</button>
           <button>Web Development</button>
+          <button>Social Media Marketing</button>
+          <button>Content Creation</button>
+          <button>Cyber Security</button>
+          <button>IT Support</button>
         </div></span>
         <div className="container">
         </div>
