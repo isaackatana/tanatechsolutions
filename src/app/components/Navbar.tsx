30dbed9c78f51cb91820532903327e96f6ba1a1e
Isaac Katana <mrisaackatana@gmail.com>
2025-05-18 22:18:33 +0300

    00

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,21 +1,44 @@
-// src/components/Navbar.tsx
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: "Services", href: "/services" },
+    { name: "Portfolio", href: "/portfolio" },
+    { name: "Blog", href: "/blog" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+  ];
+
   return (
-    <nav className="bg-primary text-black py-4 px-6 flex justify-between items-center">
-      <Link href="/" className="text-2xl font-bold">
-        Tana Tech Africa
-      </Link>
-      <div className="space-x-4">
-        <Link href="/services">Services</Link>
-        <Link href="/portfolio">Portfolio</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
-    </nav>
+    <header>
+      <nav
+        className="bg-primary text-black py-4 px-6 flex justify-between items-center"
+        aria-label="Main Navigation"
+      >
+        <Link href="/" className="text-2xl font-bold">
+          Tana Tech Africa
+        </Link>
+
+        <div className="space-x-4 hidden sm:flex">
+          {navItems.map(({ name, href }) => (
+            <Link
+              key={name}
+              href={href}
+              className={`hover:underline underline-offset-4 ${
+                pathname === href ? "font-semibold text-accent" : ""
+              }`}
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </header>
   );
 };
 
